c49231cc88f8dbfc0a8d3b04da33cea26ac21eaf
Curtis Wensley <curtis.wensley@gmail.com>
2014-06-28 11:02:37 -0700

    Cleanup: - Switch to use web server settings in the User file, so we can all have different settings. - Web.config: Add MVC 4, not 3. - Not using modernizr, so remove bundle. - update-database.sh should have executable bit

--- src/Pablo.Gallery/App_Start/BundleConfig.cs
+++ src/Pablo.Gallery/App_Start/BundleConfig.cs
@@ -23,12 +23,6 @@ namespace Pablo.Gallery
 				"~/Scripts/Site.js"
 			));
 
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-				"~/Scripts/modernizr-*"
-			));
-
 			var bundle = new StyleBundle("~/Content/css").Include(
 			    "~/Content/colorbox.css",
 				"~/Content/bootstrap.css",
