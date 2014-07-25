727bbc96771977173da61b2b711bf8cf84bc38a5
Doug Moore <lordscarlet@idledreams.net>
2014-07-24 22:36:19 -0400

    Added 404 -- using redirect method, not my favorite

---
+++ src/Pablo.Gallery/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pablo.Gallery.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult NotFound()
+        {
+			Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+
+    }
+}
