529376ec8b882ac0435d17109e8f40c2a927bbb7
Doug Moore <lordscarlet@idledreams.net>
2015-03-08 16:56:16 -0400

    Remove errant reference

--- src/Pablo.Gallery/Models/Pack.cs
+++ src/Pablo.Gallery/Models/Pack.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 
 namespace Pablo.Gallery.Models
